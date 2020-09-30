138a3383b5f8781b63c52960fab88498ad0e7c88
Larissa Sales <larissa.s.sales@outlook.com>
2020-09-30 17:44:08 -0300

    [#74] Implemented dropdown to categories

--- src/app/cadastro-receitas/cadastro-receitas.component.ts
+++ src/app/cadastro-receitas/cadastro-receitas.component.ts
@@ -5,7 +5,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
-import { ingredients } from '../models/ingredients'
+import { Ingredients } from '../models/ingredients'
+import { CategoryRecipeEnum } from '../models/category-recipe.enum';
+import { Recipe } from '../models/recipe';
 //import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -15,16 +17,22 @@ import { ingredients } from '../models/ingredients'
   })
 
   export class CadastroReceitasComponent implements OnInit{
-    ingredients = new ingredients();
+    ingredients = new Ingredients();
     dataArray = [];
+    recipe: Recipe;
+    categoryRecipeEnum = CategoryRecipeEnum;
+    categoryRecipeEnumOptions = [];
+
     constructor() { }
 
     ngOnInit(): void {
       this.dataArray.push(this.ingredients);
+
+      this.categoryRecipeEnumOptions = Object.keys(this.categoryRecipeEnum);
     }
 
-    addForm(){
-      this.ingredients=new ingredients();
+    addIngredient(){
+      this.ingredients = new Ingredients();
       this.dataArray.push(this.ingredients);
 
       console.log(this.dataArray);
