9b7b29ec97e91b49a9c8db220923f5480dd553ca
Sam Buters <sambuters@gmail.com>
2020-10-15 16:44:17 -0300

    Adding backend fetching and fixing style bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { SearchService } from 'src/app/services/search.service';
 
 @Component({
@@ -10,14 +11,21 @@ export class AppComponent {
   title = 'app';
   searchTerm : string;
 
-  constructor(private searchService: SearchService) {  }
+  constructor(private searchService: SearchService, private router: Router) { }
 
   ngOnInit() {
   	this.searchService.sharedTerm.subscribe(searchTerm => this.searchTerm = searchTerm);
   }
 
   onKey(e: any) {
-  	if(e.key == "Enter")
-  		this.searchService.nextTerm(e.target.value);
+  	if(e.key == "Enter") {
+  		if(this.router.url.includes('/pesquisa')) {
+  			this.searchService.nextTerm(e.target.value);
+  		}
+
+  		else {
+  			this.router.navigateByUrl(`/pesquisa?q=${e.target.value}`);
+  		}
+  	}
   }
 }
--- src/app/pesquisa/pesquisa.component.ts
+++ src/app/pesquisa/pesquisa.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import results from '../mock/results.json';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import {SearchService} from 'src/app/services/search.service';
 
 @Component({
@@ -9,18 +10,36 @@ import {SearchService} from 'src/app/services/search.service';
 })
 export class PesquisaComponent implements OnInit { 
   searchTerm : string;
-  resultsArray = results;
-  selectedTab = 2;
-  targetTab = results[this.selectedTab];
+  resultsArray = [];
+  selectedTab = 0;
+  targetTab = {results: []};
   
-  constructor(private searchService: SearchService) {  }
+  constructor(private searchService: SearchService, private router: Router, private http: HttpClient) {  }
     
   ngOnInit(): void {
-  	this.searchService.sharedTerm.subscribe(searchTerm => this.searchTerm = searchTerm);
+  	this.searchService.sharedTerm.subscribe(searchTerm => {
+  		if(searchTerm) {
+	  		this.searchTerm = searchTerm;
+	  		this.updateResults();
+  		}
+  	});
+
+  	let q;
+  	if((q = this.router.url.match(/\?q=(.*)/)[1])) {
+  		this.searchService.nextTerm(q);
+  		this.updateResults();
+  	}
   } 
 
   changeTab(tab) {
   	this.selectedTab = tab;
-  	this.targetTab = results[this.selectedTab];
+  	this.targetTab = this.resultsArray[this.selectedTab];
+  }
+
+  updateResults() {
+  	this.http.get(`http://localhost:3000/search?q=${this.searchTerm}`).subscribe(res => {
+  		this.resultsArray = res;
+  		this.changeTab(0);
+  	});
   }
 }
