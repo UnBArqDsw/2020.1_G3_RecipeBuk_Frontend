ca74730fb20ae572d0b6e584dd12298440fe5c2e
Damaso Junio <junio.pereirab@gmail.com>
2020-11-04 19:28:56 -0300

    [#146] - Trying to fix buttons logic aaaa

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent implements OnInit {
   loading = false;
   submitted = false;
   emailIsValid = true;
+  logged = false;
 
   constructor(private accountService: AccountService, private router: Router) { }
 
@@ -31,6 +32,7 @@ export class LoginComponent implements OnInit {
     this.loading = true;
     this.accountService.login(email, password).then(() => {
       console.log('login realizado');
+      this.logged = true;
       this.router.navigate(['/']);
     }).catch((e) => {
       console.log('login falhou', e);
