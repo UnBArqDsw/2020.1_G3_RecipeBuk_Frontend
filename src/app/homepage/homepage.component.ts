d9fc4d0ce3b4836d80702cf76c6a5272d7fd098f
Sam Buters <sambuters@gmail.com>
2020-11-16 00:22:08 -0300

    [#167] Adding favorites to homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/services';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-homepage',
@@ -8,12 +10,20 @@ import { AccountService } from 'src/app/services';
 })
 export class HomepageComponent implements OnInit {
 	loggedIn : boolean;
+	favorites : any;
+	books : any;
 
-	constructor(private accountService : AccountService) {
+	constructor(private accountService : AccountService, private http : HttpClient) {
 		this.loggedIn = accountService.isLoggedIn;
 	}
 
 	ngOnInit(): void {
+		this.http.post(`${environment.apiUrl}/getFavorites`, {auth: this.accountService.userSession}).subscribe((res: any[]) => {
+			this.favorites = res;
+		});
+		
+		this.http.post(`${environment.apiUrl}/getBooks`, {auth: this.accountService.userSession}).subscribe((res: any[]) => {
+			this.books = res;
+		});
 	}
-
 }
