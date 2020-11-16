735955dfacc3d533cd95aa5b35a047fadc8f9769
Damaso Junio <junio.pereirab@gmail.com>
2020-11-16 18:54:27 -0300

    adding a new recipe and linking it

--- src/app/minhas-receitas/minhas-receitas.component.ts
+++ src/app/minhas-receitas/minhas-receitas.component.ts
@@ -36,11 +36,71 @@ export class MinhasReceitasComponent implements OnInit {
       "link": "https://www.tudoreceitas.com/receita-de-torta-de-halloween-731.html",
       "title": "Receita de Torta de Halloween",
       "img_url": "https://t1.rg.ltmcdn.com/pt/images/1/3/7/torta_de_halloween_731_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-chocolate-627.html",
+      "title": "Receita de Torta de chocolate",
+      "img_url": "https://t1.rg.ltmcdn.com/pt/images/7/2/6/img_torta_de_chocolate_627_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-holandesa-267.html",
+      "title": "Receita de Torta holandesa",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/7/6/2/img_torta_holandesa_267_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-sardinha-266.html",
+      "title": "Receita de Torta de sardinha",
+      "img_url": "https://t1.rg.ltmcdn.com/pt/images/6/6/2/img_torta_de_sardinha_266_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-bolacha-263.html",
+      "title": "Receita de Torta de bolacha",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/3/6/2/img_torta_de_bolacha_263_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-morango-262.html",
+      "title": "Receita de Torta de morango",
+      "img_url": "https://t1.rg.ltmcdn.com/pt/images/2/6/2/img_torta_de_morango_262_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-frango-202.html",
+      "title": "Receita de Torta de frango",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/2/0/2/img_torta_de_frango_202_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-liquidificador-com-agua-9717.html",
+      "title": "Receita de Torta de liquidificador com água",
+      "img_url": "https://t1.rg.ltmcdn.com/pt/images/7/1/7/torta_de_liquidificador_com_agua_9717_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-banana-com-farofa-9661.html",
+      "title": "Receita de Torta de banana com farofa",
+      "img_url": "https://t1.rg.ltmcdn.com/pt/images/1/6/6/torta_de_banana_com_farofa_9661_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-morango-com-bolacha-9683.html",
+      "title": "Receita de Torta de morango com bolacha",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/3/8/6/torta_de_morango_com_bolacha_9683_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-morango-com-bolacha-9263.html",
+      "title": "Receita de Torta de morango com bolacha",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/3/6/2/torta_de_morango_com_bolacha_9263_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-bolacha-com-chocolate-9647.html",
+      "title": "Receita de Torta de bolacha com chocolate",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/7/4/6/torta_de_bolacha_com_chocolate_9647_300_square.jpg"
+    },
+    {
+      "link": "https://www.tudoreceitas.com/receita-de-torta-de-morango-com-chantilly-9652.html",
+      "title": "Receita de Torta de morango com chantilly",
+      "img_url": "https://t2.rg.ltmcdn.com/pt/images/2/5/6/torta_de_morango_com_chantilly_9652_300_square.jpg"
     }
   ]
   recipesList2: Object = [
     {
-      "link": "https://www.tudoreceitas.com/receita-de-torta-de-chocolate-627.html",
+      "link": "/visualizar-receitas",
       "title": "Receita de Torta de chocolate",
       "img_url": "https://t1.rg.ltmcdn.com/pt/images/7/2/6/img_torta_de_chocolate_627_300_square.jpg"
     },
