0976896ce1b45539af8f9ac33628325cee8abc06
Luis Taira <luishptaira@gmail.com>
2020-11-18 18:26:09 -0300

    [#142] Adding visualize recipe parameter
    Co-authored-by: Samuel Buters <sambuters@gmail.com>

--- src/app/visualizar-receitas/visualizar-receitas.component.ts
+++ src/app/visualizar-receitas/visualizar-receitas.component.ts
@@ -5,41 +5,45 @@ import { AccountService } from 'src/app/services';
 import { Recipe } from 'src/app/models/recipe'
 import { Ingredients } from 'src/app/models/ingredients';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SearchService } from '../services/search.service';
 
 @Component({
-    selector: 'app-visualizar-receitas',
-    templateUrl: './visualizar-receitas.component.html',
-    styleUrls: ['./visualizar-receitas.component.css']
+  selector: 'app-visualizar-receitas',
+  templateUrl: './visualizar-receitas.component.html',
+  styleUrls: ['./visualizar-receitas.component.css']
 })
 
 export class VisualizarReceitasComponent implements OnInit {
-  searchRecipe : string;
-  searchIngredient : string;
-  searchPreparationMode : string;
-  searchTime : string;
-  searchYield : string;
-  searchCategory : string;
+  searchRecipe: string;
+  searchIngredient: string;
+  searchPreparationMode: string;
+  searchTime: string;
+  searchYield: string;
+  searchCategory: string;
   recipe: Recipe;
-  loggedIn : boolean;
+  loggedIn: boolean;
   ingredients = [];
+  recipeId;
 
-  constructor(private http : HttpClient, private accountService : AccountService) {
+  constructor(private http: HttpClient, private accountService: AccountService, private route : ActivatedRoute) {
     form: FormGroup;
     this.loggedIn = accountService.isLoggedIn;
     this.recipe = new Recipe();
   }
 
   //public setFormValue(value: object): void {
-    //this.formValue = value;
+  //this.formValue = value;
 
   ngOnInit(): void {
-    this.http.post(`${environment.apiUrl}/getRecipe`, {auth: this.accountService.userSession, recipeId: 1}).subscribe((res: any) => {
-        this.recipe = res.response.recipe;
-        this.ingredients = res.ingredients;
-        console.log(res)
-		});
+    this.route.params.subscribe(params => {
+      this.recipeId = params['recipeId'];
+    });
+    this.http.post(`${environment.apiUrl}/getRecipe`, { auth: this.accountService.userSession, recipeId: this.recipeId }).subscribe((res: any) => {
+      this.recipe = res.response.recipe;
+      this.ingredients = res.ingredients;
+      console.log(res)
+    });
   }
 
 }
