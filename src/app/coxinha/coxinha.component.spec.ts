1b2039a0f80ffc9a9814eb44455384eac49fe1e1
Leticia Araujo <leticiards.04@gmail.com>
2020-11-18 20:43:57 -0300

    Adding new recipe config

---
+++ src/app/coxinha/coxinha.component.spec.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-torta-morango',
+  templateUrl: './coxinha.component.html',
+  styleUrls: ['./coxinha.component.css']
+})
+export class TortaMorangoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
