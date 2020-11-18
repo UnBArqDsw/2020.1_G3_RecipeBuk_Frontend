6f09ccec5edee485b24f7aacb097da20086d3e13
Leticia Araujo <leticiards.04@gmail.com>
2020-11-17 22:25:32 -0300

    [#142] - fixing html

--- src/app/visualizar-receitas/visualizar-receitas.component.ts
+++ src/app/visualizar-receitas/visualizar-receitas.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 //import { ICadastroReceitas } from '../interfaces/cadastro-receitas';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { SearchService } from '../services/search.service';
 
 @Component({
     selector: 'app-visualizar-receitas',
@@ -15,8 +19,11 @@ export class VisualizarReceitasComponent implements OnInit {
   searchTime : string;
   searchYield : string;
   searchCategory : string;
+  resultsArray : Object = [];
+  selectedTab = 0;
+  targetTab = {results: []};
 
-  constructor() {
+  constructor(private searchService: SearchService, private router: Router, private http: HttpClient, private cdr : ChangeDetectorRef) {
     form: FormGroup;
   }
 
@@ -24,7 +31,32 @@ export class VisualizarReceitasComponent implements OnInit {
     //this.formValue = value;
 
   ngOnInit(): void {
-    console.log("Exemplo");  
+    this.searchService.sharedTerm.subscribe(searchRecipe => {
+  		if(searchRecipe) {
+	  		this.searchRecipe = decodeURIComponent(searchRecipe);
+	  		this.updateRecipes();
+  		}
+  	}); 
+  }
+
+  changeTab(tab) {
+  	this.selectedTab = tab;
+  	this.targetTab = this.resultsArray[this.selectedTab];
+  }
+
+  updateRecipes() {
+  	this.http.get(`${environment.apiUrl}/search?q=${encodeURIComponent(this.searchRecipe)}`).subscribe((res : any[]) => {
+
+  		this.resultsArray = res.filter((result) => {
+  			return !result.error;
+  		}).sort((a, b) => {
+  			if(a.name > b.name)
+  				return 1;
+  			return -1;
+  		});
+
+  		this.changeTab(0);
+  	});
   }
 
 }
