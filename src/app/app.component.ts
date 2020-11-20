2c2c0ad525731b457534e28a4600832e84e30ccb
Damaso Junio <junio.pereirab@gmail.com>
2020-11-19 22:13:54 -0300

    Cleaning the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-root',
--- src/app/cadastro-receitas/cadastro-receitas.component.ts
+++ src/app/cadastro-receitas/cadastro-receitas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../models/ingredient'
 import { CategoryRecipeEnum } from '../models/category-recipe.enum';
 import { Recipe } from '../models/recipe';
